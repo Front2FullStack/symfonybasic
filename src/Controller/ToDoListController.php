c2f9b8486ac22795e95933896c9cf54193d386d0
Sushil Parajuli <sushilparajuli2010@gmail.com>
2020-06-21 16:20:58 +0400

    basic CRUD done
    - Todo CRUD done with db

--- src/Controller/ToDoListController.php
+++ src/Controller/ToDoListController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Task;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ToDoListController extends AbstractController
@@ -12,6 +14,52 @@ class ToDoListController extends AbstractController
      */
     public function index()
     {
-        return $this->render('index.html.twig');
+        $tasks = $this->getDoctrine()->getRepository(Task::class)->findBy([], ['id' => 'DESC']);
+        return $this->render('index.html.twig', ['tasks' => $tasks]);
+    }
+
+    /**
+     * @Route("/create", name="create_task",methods={"POST"})
+     */
+    public function create(Request $request)
+    {
+        $title = trim($request->request->get('title'));
+        if (empty($title)) {
+            return $this->redirectToRoute('to_do_list');
+        }
+
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $task = new Task;
+        $task->setTitle($title);
+        $entityManager->persist($task);
+        $entityManager->flush();
+        return $this->redirectToRoute('to_do_list');
+    }
+
+    /**
+     * @Route("/switch-status/{id}", name="switch_status")
+     */
+    public function switchStatus($id)
+    {
+        // getting access without params converter
+        $entityManager = $this->getDoctrine()->getManager();
+        $task = $entityManager->getRepository(Task::class)->find($id);
+        $task->setStatus(!$task->getStatus());
+        $entityManager->flush();
+        return $this->redirectToRoute('to_do_list');
+    }
+
+    /**
+     * @Route("/delete/{id}", name="task_delete")
+     */
+    public function delete(Task $id)
+    {
+        // getting access with params converter  (Task $id) 
+        //composer require sensio/framework-extra-bundle if not installed to make params converter to work
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->remove($id); // remove will task obj through params
+        $entityManager->flush();
+        return $this->redirectToRoute('to_do_list');
     }
 }
